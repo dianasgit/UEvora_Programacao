e28b3efa5411586c3e057da42e7fff9579997d44
dianasgit <diana.git93@gmail.com>
2024-10-23 09:48:07 +0100

    lab 23.10.2024

--- p1/ap8/ex5.c
+++ p1/ap8/ex5.c
@@ -1,27 +1,27 @@
-/* Escreva um programa que pede ao utilizador para definir uma senha e depois verificar se a
-senha está correta. O utilizador tem 3 tentativas para introduzir a senha correta. Se a senha
-correta for inserida dentro das 3 tentativas, o programa deve mostrar "Acesso concedido", caso
+/* Escreva um programa que pede ao utilizador para definir uma senha e depois verificar se a senha está correta. O utilizador tem 3 tentativas para introduzir a senha correta. Se a senha correta for inserida dentro das 3 tentativas, o programa deve mostrar "Acesso concedido", caso
 contrário, o programa deve mostrar "Acesso negado".
-Regras: ● O programa deve utilizar um ciclo while ou for com uma condicional para verificar
-as tentativas. ● Se a senha correta for inserida, o ciclo deve terminar.*/
+Regras: ● O programa deve utilizar um ciclo while ou for com uma condicional para verificar as tentativas. ● Se a senha correta for inserida, o ciclo deve terminar.*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char* argv[]){
 
-    int senha, try, n;
+    char  senha[10];
+    char try[10];
+    int n;
 
     printf("Digite uma senha: ");
-    scanf("%d", &senha);
+    scanf("%s", senha);
 
     n = 1;
     while (n <4 && senha != try){
         printf("\ndigite novamente a senha\n");
-        scanf("%d", &try);
+        scanf("%s", &try);
         n = n+1;
 
-        if(try == senha){
+        if(strcmp(try, senha) == 0){
             printf("Acesso concedido");
             return 0;
         }else{
